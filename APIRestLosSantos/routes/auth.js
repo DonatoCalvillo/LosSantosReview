cb12f85bfb8d4b349146636c5de91795c4adb735
Donato Calvillo <42361463+DonatoCalvillo@users.noreply.github.com>
2021-08-10 18:53:03 -0500

    Auth verification
    Login implemented.

--- APIRestLosSantos/routes/auth.js
+++ APIRestLosSantos/routes/auth.js
@@ -6,15 +6,15 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router()
 
 router.post('/login',[
-    check('correo', 'El correo es obligatorio').isEmail(),
-    check('contrasenia', 'La contrasenia es obligatorio').not().isEmpty(),
+    check('email', 'The email is required').isEmail(),
+    check('password', 'The password is required').not().isEmpty(),
     validarCampos
 ] , login );
 
-router.post('/google',[
-    check('id_token', 'El id token es obligatorio').not().isEmpty(),
-    validarCampos
-] , googleSignIn );
+// router.post('/google',[
+//     check('id_token', 'El id token es obligatorio').not().isEmpty(),
+//     validarCampos
+// ] , googleSignIn );
 
 
 
